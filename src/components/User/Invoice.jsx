c16b54f5218b15eb114306e8a1e539699e514779
S520H0531 <520H0531@student.tdtu.edu.vn>
2023-12-09 12:47:06 +0700

    continue fix error

--- src/components/User/Invoice.jsx
+++ src/components/User/Invoice.jsx
@@ -1,5 +1,5 @@
 import pdfMake from 'pdfmake/build/pdfmake'
-import pdfFonts from 'pdfmake/build/vfs_fonts'
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js'
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { apiUpdateProfile } from '../../apis'
