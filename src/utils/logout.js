e412527647d3fd3cc80166c21650e42fbfbda93c
S520H0531 <520H0531@student.tdtu.edu.vn>
2023-12-09 13:31:23 +0700

    fixed

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -1,5 +1,7 @@
 import path from './path'
 export const logout = () => {
-	window.location.replace(`https://frontend-pos-three.vercel.app/${path.LOGIN}`)
+	window.location.replace(
+		`https://frontend-pos-nhathaohuynh.vercel.app/${path.LOGIN}`,
+	)
 	localStorage.clear()
 }
